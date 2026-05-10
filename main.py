b860a6acdf7a3ff0c5387fdb4c1b2bc262e52f37
kerdouueni <kerdouueni@gmail.com>
2026-05-10 20:24:22 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from character import Hero, Villain
+from file_utils import get_random_from_file
+
+hero_name = get_random_from_file(r"data\heroes.txt")
+hero_weapon = get_random_from_file(r"data\weapons.txt")
+villain_name = get_random_from_file(r"data\villains.txt")
+villain_weapon = get_random_from_file(r"data\weapons.txt")
+
+hero = Hero(hero_name, hero_weapon)
+villain = Villain(villain_name, villain_weapon)
+
+print("=== BATTLE BEGINS ===")
+
+print(f"Hero: {hero.name} | HP: {hero.hp} | Weapon: {hero.weapon}")
+print(f"Villain: {villain.name} | HP: {villain.hp} | Weapon: {villain.weapon}")
+
+round_number = 1
+
+while hero.is_alive() and villain.is_alive():
+    print(f"---Round {round_number} ---")
+
+    hero.attack(villain)
+
+    if not villain.is_alive():
+        break
+
+    villain.attack(hero)
+
+    round_number = round_number + 1
+
+print("=== BATTLE ENDS ===")
+
+if hero.is_alive():
+    print("Hero saves the day!")
+else:
+    print("Dark side wins!")
